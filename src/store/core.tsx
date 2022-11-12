02ee46efa1421d847bde28dc1724050a2e57ec6d
สิทธา รักษาสวัสดิ์ <sittha.raksasawat@gmail.com>
2022-11-13 04:56:11 +0700

    fix for build

--- src/store/core.tsx
+++ src/store/core.tsx
@@ -3,36 +3,36 @@ import {
   ThunkAction,
   combineReducers,
   configureStore,
-} from "@reduxjs/toolkit";
+} from '@reduxjs/toolkit'
 
-import { authSlice } from "./auth";
+import { authSlice } from './auth'
 
 const reducers = combineReducers({
   auth: authSlice.reducer,
-});
+})
 
-let persistedState = {};
-if (typeof window !== "undefined") {
-  persistedState = localStorage.getItem("root")
-    ? JSON.parse(localStorage.getItem("root") as string)
-    : {};
+let persistedState = {}
+if (typeof window !== 'undefined') {
+  persistedState = localStorage.getItem('root')
+    ? JSON.parse(localStorage.getItem('root') as string)
+    : {}
 }
 
 export const reduxStoreMain = configureStore({
   preloadedState: persistedState,
   reducer: reducers,
-  devTools: process.env.NODE_ENV !== "production",
-});
+  devTools: process.env.NODE_ENV !== 'production',
+})
 
 reduxStoreMain.subscribe(() => {
-  localStorage.setItem("root", JSON.stringify(reduxStoreMain.getState()));
-});
+  localStorage.setItem('root', JSON.stringify(reduxStoreMain.getState()))
+})
 
 export type AppDispatch = typeof reduxStoreMain.dispatch;
 export type RootState = ReturnType<typeof reduxStoreMain.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+ReturnType,
+RootState,
+unknown,
+Action<string>
 >;
